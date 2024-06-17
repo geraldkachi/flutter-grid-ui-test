f19e72b6c9ac7b969bb6932971e758dcad41bfe6
geraldkachi <fitzgeraldkachi@gmail.com>
2024-06-17 11:46:06 +0100

    latest

--- lib/conponents/my_darwer.dart
+++ lib/conponents/my_darwer.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
@@ -7,61 +8,58 @@ class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-        // width: MediaQuery.of(context).size.width * 0.9, //<-- SEE HERE
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
+      // width: MediaQuery.of(context).size.width * 0.9, //<-- SEE HERE
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.grey,
-              ),
-              child: Text('Drawer Header'),
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                 Column(
+            Column(
+              children: [
+                Column(
                   children: [
-                     ListTile(
-                leading: const Icon(
-                  Icons.home,
-                ),
-                title: const Text('Page 1'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: const Icon(
-                  Icons.train,
-                ),
-                title: const Text('Page 2'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
+                     DrawerHeader(
+                      decoration: const BoxDecoration(
+                        // color: Colors.grey,
+                      ),
+                      child: Icon(Icons.favorite, color: Theme.of(context).colorScheme.inversePrimary,)
+                    ),
+                    ListTile(
+                      leading: const Icon(
+                        Icons.home,
+                      ),
+                      title: const Text('Home'),
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(
+                        Icons.train,
+                      ),
+                      title: const Text('Flutter Crud'),
+                      onTap: () {
+                        Navigator.pop(context);
+                        Navigator.pushNamed(context, '/crud');
+                      },
+                    ),
                   ],
-                 ),
-              
-              
-              ListTile(
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
                 leading: const Icon(
                   Icons.logout_rounded,
                 ),
                 title: const Text('Log Out'),
-                onTap: () async{
+                onTap: () async {
                   // Navigator.pop(context);
-                    await FirebaseAuth.instance.signOut();
+                  await FirebaseAuth.instance.signOut();
                 },
               ),
-                ],
-              ),
-            )
-          ],
-        ),
-      );
+            ),
+          ]),
+    );
   }
 }
